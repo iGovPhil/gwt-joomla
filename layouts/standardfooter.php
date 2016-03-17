34282d34b2785c1de6abdadb3cb556e2b4bb1424
igov <jonathan147cn@gmail.com>
2016-03-17 14:57:32 +0800

    update standard footer link

--- layouts/standardfooter.php
+++ layouts/standardfooter.php
@@ -6,7 +6,7 @@
       var js, gjs = d.getElementById('gwt-standard-footer');
 
       js = d.createElement(s); js.id = id;
-      js.src = "http://gwhs.i.gov.ph/gwt-footer/footer.js";
+      js.src = "//gwhs.i.gov.ph/gwt-footer/footer.js";
       gjs.parentNode.insertBefore(js, gjs);
 
     }(document, 'script', 'gwt-footer-jsdk'));
